2ac3d29e12a391a7a96d6992f8b33a62439c930d
iceyy-TI <80018771+iceyy-TI@users.noreply.github.com>
2021-03-04 11:11:23 +0700

    Looping 2 variable

--- Tugas_Meeting2.py
+++ Tugas_Meeting2.py
@@ -1,4 +1,16 @@
-var1 = 3
-var2 = 4
+while True : // ini akan menjadi perulangan berulang kali
+  var1 = int(input("Berikan Variable ke-1 : ")) // ini akan meminta inputan berupa integer
+  var2 = int(input("Berikan Variable ke-2 : ")) // sama seperti sebelumnya
+  
+  print(var1 / var2)
+  
+  //kita perlu membuat perintah agar perulangan di hentikan
+  print("Hentikan perulangan?) 
+  close = input(Ketik 'Y' pada kotak input untuk menghentikan perulangan)")
+  if close == 'Y' or close == 'y':
+        break
+        
+  // dengan ini perulangan dapat berhenti jiak user mengetikkan Y atau y
 
-print(var1 / var2)
+
+
