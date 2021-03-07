f4baed10d8da7ec366797ee5dd085cd13046ca93
iceyy-TI <80018771+iceyy-TI@users.noreply.github.com>
2021-03-07 19:43:32 +0700

    Tugas_Meeting2_Finale

--- Tugas_Meeting2.py
+++ Tugas_Meeting2.py
@@ -1,16 +1,32 @@
-while True : // ini akan menjadi perulangan berulang kali
-  var1 = int(input("Berikan Variable ke-1 : ")) // ini akan meminta inputan berupa integer
-  var2 = int(input("Berikan Variable ke-2 : ")) // sama seperti sebelumnya
+class Looping:   
+    def __init__(self):
+        pass
+    def kali(var1, var2):
+        return var1 * var2
   
-  print(var1 / var2)
+    def bagi(var1, var2):
+        return var1 / var2
   
-  //kita perlu membuat perintah agar perulangan di hentikan
-  print("Hentikan perulangan?) 
-  close = input(Ketik 'Y' pada kotak input untuk menghentikan perulangan)")
+    def jumlah(var1, var2):
+        return var1 + var2
+  
+    def kurang(var1, var2):
+        return var1 - var2
+
+while True : # ini akan menjadi perulangan berulang kali
+  var1 = int(input("Berikan Variable ke-1 : ")) # ini akan meminta inputan berupa integer
+  var2 = int(input("Berikan Variable ke-2 : ")) # sama seperti sebelumnya
+  
+  print("Kali : ", Looping.kali(var1,var2))
+  print("Bagi : ", Looping.bagi(var1,var2))
+  print("Tambah : ", Looping.jumlah(var1,var2))
+  print("Kurang : ", Looping.kurang(var1,var2))
+  
+  #kita perlu membuat perintah agar perulangan di hentikan
+  print("Hentikan perulangan?") 
+  close = input("Ketik 'Y' pada kotak input untuk menghentikan perulangan")
   if close == 'Y' or close == 'y':
         break
         
-  // dengan ini perulangan dapat berhenti jiak user mengetikkan Y atau y
-
-
+  # dengan ini perulangan dapat berhenti jiak user mengetikkan Y atau y
 
